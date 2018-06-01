4c85979c734f27df3f6080d4f54695623807e4ee
Ben Robinson <mentionbenjamin@gmail.com>
2018-06-01 21:45:22 +0100

    remove song function test added

--- room.rb
+++ room.rb
@@ -1,23 +1,29 @@
 class Room
 
-  attr_reader(:name, :tracklist, :guests_in_room)
+  attr_reader(:name, :till_balance, :tracklist, :guests_in_room)
 
-  def initialize(name, tracklist)
+  def initialize(name, till_balance, tracklist)
     @name = name
+    @till_balance = till_balance
     @tracklist = tracklist
     @guests_in_room = []
   end
 
-  # def add_guest_to_room(guest)
-  #   @guests_in_room.push(guest)
-  # end
-  # #
-  # # def number_of_guests()
-  # #   return @guests_in_room.count()
-  # # end
+  def add_guest_to_room(guest)
+    @guests_in_room.push(guest)
+  end
 
+  def number_of_guests()
+    return @guests_in_room.count()
+  end
 
+  def add_song(song)
+    @tracklist.push(song)
+  end
 
+  def remove_song(song)
+    @tracklist.delete(song)
+  end
 
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -10,12 +10,13 @@ class RoomTest < MiniTest::Test
     @song1 = Song.new("Cher - Believe")
     @song2 = Song.new("Diana Ross - It's My House")
     @song3 = Song.new("Whitney Housten - How Will I Know")
+    @song4 = Song.new("Come Fly With Me - Frank Sinatra")
     @tracklist = [@song1, @song2, @song3]
     @guest1 = Guest.new("Ben", 30)
     @guest2 = Guest.new("Daniel", 20)
     @guest3 = Guest.new("James", 40)
 
-    @room = Room.new("Pop Karaoke", tracklist)
+    @room = Room.new("Pop Karaoke", 100, @tracklist)
   end
 
 
@@ -27,18 +28,20 @@ class RoomTest < MiniTest::Test
     assert_equal([@song1, @song2, @song3], @room.tracklist())
   end
 
-  # def test_check_in_guest_to_room
-  #   @room.add_guest_to_room(@guest1)
-  #
-  #   assert_equal("Ben", @room.guests_in_room[0].name)
-  #   #
-  #   # assert_equal(1, @room.number_of_guests())
-  #   #
-  # end
-
+  def test_check_in_guest_to_room
+    @room.add_guest_to_room(@guest1)
+    assert_equal("Ben", @room.guests_in_room[0].name)
+    # assert_equal(1, @room.number_of_guests())
+  end
 
   def test_add_song_to_room
-    assert_equal([@song1], @tracklist.)
+    @room.add_song(@song4)
+    assert_equal(4, @tracklist.length())
+  end
+
+  def test_remove_song_from_room
+    @room.remove_song(@song2)
+    assert_equal(2, @tracklist.length())
   end
 
 
