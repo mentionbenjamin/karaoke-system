0ac94ddead41d54b17d7016c04dac87c87f06228
Ben Robinson <mentionbenjamin@gmail.com>
2018-06-03 10:01:37 +0100

    working on adding room capacity function

--- room.rb
+++ room.rb
@@ -1,30 +1,38 @@
 class Room
 
-  attr_reader(:name, :till_balance, :tracklist, :guests_in_room)
+  attr_reader(:name, :till_balance, :tracklist, :capacity, :guests_in_room)
 
-  def initialize(name, till_balance, tracklist)
+  def initialize(name, till_balance, tracklist, capacity)
     @name = name
     @till_balance = till_balance
     @tracklist = tracklist
+    @capacity = capacity
     @guests_in_room = []
   end
-
+  # ADD GUEST TO ROOM
   def add_guest_to_room(guest)
     @guests_in_room.push(guest)
   end
-
+  # REMOVE GUEST FROM ROOM
+  def remove_guest_from_room(guest)
+    @guests_in_room.delete(guest)
+  end
+  # COUNT NUMBER OF GUESTS IN ROOM ARRAY
   def number_of_guests()
     return @guests_in_room.count()
   end
-
+  # ADD SONG TO TRACKLIST
   def add_song(song)
     @tracklist.push(song)
   end
-
+  # REMOVE SONG FROM TRACKLIST
   def remove_song(song)
     @tracklist.delete(song)
   end
-
+  # ROOM AT CAPACITY YES/NO
+  def room_at_capacity?()
+    return @guests_in_room >= 5
+  end
 
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -12,41 +12,55 @@ class RoomTest < MiniTest::Test
     @song3 = Song.new("Whitney Housten - How Will I Know")
     @song4 = Song.new("Come Fly With Me - Frank Sinatra")
     @tracklist = [@song1, @song2, @song3]
+
     @guest1 = Guest.new("Ben", 30)
     @guest2 = Guest.new("Daniel", 20)
     @guest3 = Guest.new("James", 40)
 
-    @room = Room.new("Pop Karaoke", 100, @tracklist)
+    @room = Room.new("Pop Karaoke", 100, @tracklist, 3)
   end
 
-
+  # CHECK ROOM NAME
   def test_room_name
     assert_equal("Pop Karaoke", @room.name())
   end
-
+  # CHECK TRACKLIST
   def test_tracklist
     assert_equal([@song1, @song2, @song3], @room.tracklist())
   end
-
-  def test_check_in_guest_to_room
+  # ADD GUEST TO ROOM
+  def test_add_guest_to_room
     @room.add_guest_to_room(@guest1)
-    assert_equal("Ben", @room.guests_in_room[0].name)
-    # assert_equal(1, @room.number_of_guests())
+    assert_equal(1, @room.number_of_guests())
   end
-
+  # REMOVE GUEST FROM ROOM
+  def test_remove_guest_from_room
+    @room.add_guest_to_room(@guest1)
+    @room.remove_guest_from_room(@guest1)
+    assert_equal(0, @room.number_of_guests())
+  end
+  # ADD SONG TO ROOM TRACKLIST
   def test_add_song_to_room
     @room.add_song(@song4)
     assert_equal(4, @tracklist.length())
   end
-
+  # REMOVE SONG FROM TRACKLIST
   def test_remove_song_from_room
     @room.remove_song(@song2)
     assert_equal(2, @tracklist.length())
+  end
+  #  CHECK ROOM IS NOT AT CAPACITY
+  def test_room_at_capacity__return_true
+    @room.add_guest_to_room(@guest1) #adding guest to room
+    assert_equal(true, @room.room_at_capacity?()) #at capacity yes/no
   end
 
 
 
 
+  def customer_too_drunk?(customer)
+    return customer.drunkenness >= 50
+  end
 
 
 
